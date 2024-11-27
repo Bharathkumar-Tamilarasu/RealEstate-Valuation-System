f2c3a0eb7b9b4d1d687ed8115551609f56bd2c89
Bharathkumar-Tamilarasu <bharathkumar.t.17@gmail.com>
2024-11-27 21:47:31 +0530

    artifacts paths changes

--- flask_app/server/util.py
+++ flask_app/server/util.py
@@ -12,42 +12,42 @@ def artifacts_loader():
     global _bath
     global _all_columns
 
-    # abs_path =  os.path.abspath(__file__)
+    abs_path =  os.path.abspath(__file__)
 
     # Loading the model
-    # model_path = os.path.join(abs_path,"../../../artifacts\House Price Prediction_Pickle.pickle")
-    # model_path_norm = os.path.normpath(model_path)
+    model_path = os.path.join(abs_path,"..","..","..","artifacts","House Price Prediction_Pickle.pickle")
+    model_path_norm = os.path.normpath(model_path)
 
-    # with open(model_path_norm,"rb",) as f:
-    #     _model = pickle.load(f)
+    with open(model_path_norm,"rb",) as f:
+        _model = pickle.load(f)
 
-    model_url = "https://raw.githubusercontent.com/Bharathkumar-Tamilarasu/RealEstate-Valuation-System/main/artifacts/House%20Price%20Prediction_Pickle.pickle"
+    # model_url = "https://raw.githubusercontent.com/Bharathkumar-Tamilarasu/RealEstate-Valuation-System/main/artifacts/House%20Price%20Prediction_Pickle.pickle"
 
     # Fetch the pickle file
-    model_response = requests.get(model_url)
+    # model_response = requests.get(model_url)
 
     # Check if the request was successful
-    if model_response.status_code == 200:
-        # Load the pickle file from the response content
-        _model = pickle.load(BytesIO(model_response.content))
-    else:
-        print(f"Failed to fetch the pickle file, status code: {model_response.status_code}")
+    # if model_response.status_code == 200:
+    #     # Load the pickle file from the response content
+    #     _model = pickle.load(BytesIO(model_response.content))
+    # else:
+    #     print(f"Failed to fetch the pickle file, status code: {model_response.status_code}")
 
     # Loading input parameters for form processing and prediction.
-    # input_path = os.path.join(abs_path,"../../../artifacts\prediction_input.json")
-    # input_path_norm = os.path.normpath(input_path)
-    # with open(input_path_norm,'rb') as f:
-    #     _all_inputs = json.load(f)
+    input_path = os.path.join(abs_path,"..","..","..","artifacts","prediction_input.json")
+    input_path_norm = os.path.normpath(input_path)
+    with open(input_path_norm,'rb') as f:
+        _all_inputs = json.load(f)
 
-    allinputs_url = "https://raw.githubusercontent.com/Bharathkumar-Tamilarasu/RealEstate-Valuation-System/main/artifacts/prediction_input.json"
-    allinputs_response = requests.get(allinputs_url)
+    # allinputs_url = "https://raw.githubusercontent.com/Bharathkumar-Tamilarasu/RealEstate-Valuation-System/main/artifacts/prediction_input.json"
+    # allinputs_response = requests.get(allinputs_url)
 
-    # Check if the request was successful
-    if allinputs_response.status_code == 200:
-     # Load the pickle file from the response content
-        _all_inputs = allinputs_response.json()
-    else:
-        print(f"Failed to fetch the all_inputs file, status code: {allinputs_response.status_code}")
+    # # Check if the request was successful
+    # if allinputs_response.status_code == 200:
+    #  # Load the pickle file from the response content
+    #     _all_inputs = allinputs_response.json()
+    # else:
+    #     print(f"Failed to fetch the all_inputs file, status code: {allinputs_response.status_code}")
     
     
     _locations = _all_inputs['locations']
